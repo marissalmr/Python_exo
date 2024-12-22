c023adaeefe65bcc24131bdd0a90c0c8eb326796
Marissa Lamri <marielnrtstu@gmail.com>
2024-12-22 13:54:03 +0100

    add gitignore file + pair exos

---
+++ exos_pair.py
@@ -0,0 +1,11 @@
+def is_pair(n) :
+    if n % 2 == 0 :
+        return True
+
+    return False
+
+
+for i in range(2,21) :
+    result = is_pair(i)
+    if result is True :
+        print(f"{i} est pair")
