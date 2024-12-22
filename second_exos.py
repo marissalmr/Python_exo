52e231e542ec2703da668707f0d0ceff2300e5e9
Marissa Lamri <marielnrtstu@gmail.com>
2024-12-22 15:14:32 +0100

    Bonjour

--- second_exos.py
+++ second_exos.py
@@ -1,1 +1,12 @@
 # second exos file
+from itertools import count
+
+
+phrase = input("Donnez moi une phrase: ")
+voyelles = ["a","e","i","o","u","y"]
+
+count = 0
+for car in count:
+    if car in voyelles:
+        count = count + 1
+print(count)
