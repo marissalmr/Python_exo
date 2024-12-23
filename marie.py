10072b3a2c8ba46635a8d6a2ae23f0f056c0b915
Marissa Lamri <marielnrtstu@gmail.com>
2024-12-23 22:25:00 +0100

    Jeux pierre feuille ciseau

---
+++ marie.py
@@ -0,0 +1,22 @@
+import random
+
+from functions_custom import choiix
+
+def choiix(utilisateur, aleatoire) :
+    """
+    :param utilisateur: ce parametre definit le choix de l'utilisateur'
+    :param aleatoire: ce parametre definit le choix du pc'
+    :return: string qui contient la reponse du fight
+    """
+    print(aleatoire)
+    if aleatoire == "Pierre" and utilisateur == "Ciseau":
+        return "Vous avez perdu"
+    elif aleatoire == "Feuille" and utilisateur == "Ciseau":
+        return "Vous avez gagner"
+
+a = input("Choisisez Pierre, Feuille ou Ciseau")
+choix = ["Pierre", "Feuille", "Ciseau"]
+b = random.choice(choix)
+resultat = choiix(utilisateur=a, aleatoire=b)
+print(resultat)
+
